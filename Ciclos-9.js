e6b5f3997309a9c79a2d3ef34b914ea28d352820
Luciano <lucasacci@hotmail.com>
2022-07-13 14:00:37 -0300

    ejercicio 9 y 10

--- Ciclos-9.js
+++ Ciclos-9.js
@@ -1,31 +1,20 @@
 
-let m = prompt("Ingrese la cantidad de filas");
-let n = prompt("Ingrese la cantidad de columnas");
-let x = m*n;
+let n = prompt("Ingrese la cantidad de numeros (MENOR A 50)");
 
-if(Number(m)==m && Number(n)==n){
-    m = Number(m);
+if(Number(n)==n && n < 50){
     n = Number(n);
+
+    for (let i = 0; i <= n; i++) {
+        for (let j = 0; j < i; j++) {
+             document.write(i);
+        }
+        document.write("<br>");
+     }
 }
 else{
     alert(`Ingrese un numero valido`);
 }
 
 
-document.write(`<table>
-<tbody>`)
-
-for (let i = 0; i < m; i++) {
-    
-    document.write(`<tr>`)
-        for (let j = 0; j < n; j++) {
-        document.write(`<td>`+ x + `</td>`)
-        x--;
-    }
-    
-    document.write(`</tr>`);
-}
-
-
-document.write(`</tbody>
-</table>`)
+ 
+ 
